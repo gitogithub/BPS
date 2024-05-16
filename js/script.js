40da5944b3aded0fe9ea81de5c0fb3e05e3847ec
gitogithub <47066076+gitogithub@users.noreply.github.com>
2024-05-16 08:50:08 +0200

    Batch update

---
+++ js/script.js
@@ -0,0 +1,4 @@
+
+
+    window.silex = window.silex || {}
+    window.silex.data = {"site":{"width":1200},"pages":[{"id":"page-home","displayName":"home","link":{"linkType":"LinkTypePage","href":"#!page-home"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
